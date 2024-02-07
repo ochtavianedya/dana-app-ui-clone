b73c618f4cffcf8d225b688a17a1b2e9d2577181
ochtavianedya <edyapratama712@gmail.com>
2024-02-07 21:03:08 +0700

    add theme config and dependencies

---
+++ lib/shared/theme/theme.dart
@@ -0,0 +1,18 @@
+import 'package:dana_app_ui_clone/shared/theme/theme_constant.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getDefaultTheme() {
+  return ThemeData(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primaryColor,
+      foregroundColor: Colors.white,
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Colors.white,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white,
+    ),
+    scaffoldBackgroundColor: Colors.white,
+  );
+}
